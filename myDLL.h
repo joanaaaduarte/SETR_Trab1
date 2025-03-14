13e5f1e2e3d971678e4b9cce949b7871750fa896
Hugo <hugojpmiranda@ua.pt>
2025-03-14 00:44:36 +0000

    Função init & insert feitas

--- myDLL.h
+++ myDLL.h
@@ -1,15 +1,34 @@
 #include <stdlib.h>
-
-MyDLLInit();
-
-MyDLLInsert();
-
-MyDLLRemove(){
-}
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
 
-MyDLLFind();
+#define tamanho_dados 60
+#define numero_nos 3
 
-MyDLLFindNext();
+typedef struct 
+{
+    uint16_t chave_no;                    /** identifica o nó*/
+    unsigned char dados_no[tamanho_dados]; /** dados do nó*/
+    int no_anterior;                       /** indice do nó anterior*/ 
+    int proximo_no;                        /** indice do proximo nó*/
+}no;
 
-MyDLLFindPrevious();
+typedef struct 
+{
+    no array_nos [numero_nos];              /** array de nós*/
+    int no_atual;                           /** indice do nó atual*/
+    int primeiro_no;                        /** indice do primeiro no*/
+    int ultimo_no;                          /** indece do ultimo no*/
+    int numero_no;                         /** numero total de nós*/
+    no  *p_no_atual;                         /** ponteiro para nó atual*/
+}lista;
+
+//Funções auxiliares
+void ValidarChave(uint16_t *chave);
+void ValidarDados(unsigned char *dados);
+void LimparBuffer();
+
+void MyDLLInit(lista *DLL);
+void MyDLLInsert(lista *lst, uint16_t chave, const unsigned char *dados);
